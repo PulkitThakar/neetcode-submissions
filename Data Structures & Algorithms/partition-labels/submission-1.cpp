5372e6a2df2fccc7265396d8a0ee692ce60843a5
Pulkit Thakar <54314577+PulkitThakar@users.noreply.github.com>
2026-06-15 16:57:22 -0500

    Add: partition-labels - submission-1

---
+++ Data Structures & Algorithms/partition-labels/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        unordered_map<char, int> countMap;
+        for(char c: s) {
+            countMap[c] += 1;
+        }
+
+        int n = s.size();
+        int l = 0;
+        unordered_set<char> currCharSet;
+        vector<int> res;
+        for(int r = 0; r < n; r++) {
+            currCharSet.insert(s[r]);
+            countMap[s[r]]--;
+            if(countMap[s[r]] == 0)
+                currCharSet.erase(s[r]);
+            
+            if(currCharSet.empty()){
+                res.push_back(r - l + 1);
+                l = r + 1;
+            }
+        }
+        return res;
+        
+    }
+};
