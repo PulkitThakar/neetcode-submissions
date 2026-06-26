db3e18d7fa48d99f01fa661d84a4b30c6d35ad16
Pulkit Thakar <54314577+PulkitThakar@users.noreply.github.com>
2026-06-26 15:48:56 -0500

    Add: valid-parenthesis-string - submission-2

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        minn = 0
+        maxx = 0
+        
+        for i in s:
+            if i == '(':
+                minn += 1
+                maxx += 1
+            elif i == ")":
+                minn -= 1
+                minn = max(minn, 0)
+                maxx -= 1
+                if maxx < 0:
+                    return False
+            else:
+                minn -= 1
+                minn = max(minn, 0)
+                maxx += 1
+        
+        return minn == 0
+            
