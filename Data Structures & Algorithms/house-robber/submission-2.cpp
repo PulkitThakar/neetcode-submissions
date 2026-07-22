93ff48cf834e115b49c7a0f696bcd179ac922aca
Pulkit Thakar <54314577+PulkitThakar@users.noreply.github.com>
2026-07-22 06:14:35 -0500

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int last = 0, prev = 0;
+
+        for(int i: nums) {
+            int temp = max(prev + i, last);
+            prev = last;
+            last = temp;
+        }
+
+        return max(last, prev);
+    }
+};
