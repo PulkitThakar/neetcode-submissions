47a468de26fbba512fa5c4ac1619ec1a36ae6da5
Pulkit Thakar <54314577+PulkitThakar@users.noreply.github.com>
2026-08-25 17:34:36 -0500

    Add: hand-of-straights - submission-0

---
+++ Data Structures & Algorithms/hand-of-straights/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        priority_queue<int, std::vector<int>, std::greater<int>> pq;
+        unordered_map<int, int> m;
+        for(int i: hand) {
+            pq.push(i);
+            m[i] += 1;
+        }
+
+        while(!pq.empty()) {
+            int start = pq.top(); pq.pop();
+            if(m[start] == 0)
+                continue;
+            
+            for(int i = 0; i < groupSize; i++) {
+                int k = start + i;
+                if(!m.count(k) or m[k] == 0)
+                    return false;
+                m[k]--;
+            }
+        }
+
+        return true;
+    }
+};
