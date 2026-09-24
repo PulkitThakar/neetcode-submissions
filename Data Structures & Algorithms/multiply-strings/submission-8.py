717beb261dbe458918a8ce070546a9f4ff46cf71
Pulkit Thakar <54314577+PulkitThakar@users.noreply.github.com>
2026-09-24 05:51:07 -0500

    Add: multiply-strings - submission-8

---
+++ Data Structures & Algorithms/multiply-strings/submission-8.py
@@ -0,0 +1,24 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        res_len = (len(num1) + len(num2))
+        res = [0] * res_len
+
+        for i in range(len(num1) - 1, -1, -1):
+            for j in range(len(num2) - 1, -1, -1):
+                k = i + j + 1
+                z = int(num1[i]) * int(num2[j])
+                z += res[k]
+                res[k] = z % 10
+                z = z // 10
+                if z:
+                    res[k-1] += z
+
+                
+        r = ""
+        i = 0
+        while i < res_len and res[i] == 0:
+            i += 1
+        while i < res_len:
+            r += str(res[i])
+            i += 1
+        return r if r else "0"
